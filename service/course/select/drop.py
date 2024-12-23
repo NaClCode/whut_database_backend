ff9e2b508c361540c5e397d212f0c7271ff71eec
NaCl <120160850+NaClCode@users.noreply.github.com>
2024-12-23 15:22:30 +0800

    🐞 fix(select): 修复退课的问题

--- service/course/select/drop.py
+++ service/course/select/drop.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from schema.course.select.CourseDropSchema import CourseDropSchema
 from crud.EnrollCrud import EnrollCrud
-
+from config import config
 from utils.auth_token import validate_student_token
 from utils.get_db import get_db
 from datetime import datetime
@@ -16,8 +16,19 @@ async def _(body:CourseDropSchema, token_payload: dict = Depends(validate_studen
     user_id = token_payload.get("user_id")
     id = body.classid
     time = datetime.now()
+    select_start_time = config.select_start_time
+    select_end_time = config.select_end_time
 
     try:
+        if time < select_start_time or time > select_end_time:
+            return JSONResponse(
+                status_code=400,
+                content={
+                    "status": 1,
+                    "message": "不在选课时间",
+                }
+            )
+        
         EnrollCrud.drop_course(db, student_id=user_id, class_id=id, time=time)
     except Exception as e:
         traceback.print_exc()
